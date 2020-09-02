bb5250389d1487d8ef1cc501573d72848d6e0015
Rushad Mehta <rushadmehta16@gmail.com>
2020-09-02 17:45:00 +0530

    Added conditions for mesh analysis

--- electrosim.py
+++ electrosim.py
@@ -6,49 +6,104 @@ Created on Tue Aug 25 20:25:08 2020
 """
 
 import numpy as np
-
-class Source():
+        
+class CurrentSource():
     
-    def __init__(self):
-        self._t = np.arange(0, 20, 0.01)
-
-    class CurrentSource():
-        
-        def __init__(self):
-            pass
-        
-        def DC(self, current):
-            self._Idc = current*np.ones(20)
-        
-        def AC(self, peak_current, angular_freq, phase_angle):        
+    class DC():
+        def __init__(self, current):
+            self._I = current*np.ones(len(np.arange(0, 20, 0.01)))
+    
+    class AC():        
+        def __init__(self, peak_current, angular_freq, phase_angle):
+            self._t = np.arange(0, 20, 0.01)
             self._angular_freq = angular_freq
             self._phase_angle = phase_angle
-            self._Iac = peak_current*np.sin(self.angular_freq*self.t + self.phase_angle)
+            self._I = peak_current*np.sin(self._angular_freq*self._t + self._phase_angle)
+            
+class VoltageSource():
     
-    class VoltageSource():
-        
-        def __init__(self):
-            pass
-        
-        def DC(self, voltage):
-            self._Vdc = voltage*np.ones(20)
-        
-        def AC(self, peak_voltage, angular_freq, phase_angle):        
+    class DC():
+        def __init__(self, voltage):
+            self._V = voltage*np.ones(len(np.arange(0, 20, 0.01)))
+    
+    class AC():        
+        def __init__(self, peak_voltage, angular_freq, phase_angle):
+            self._t = np.arange(0, 20, 0.01)
             self._angular_freq = angular_freq
             self._phase_angle = phase_angle
-            self._Vac = peak_voltage*np.sin(self.angular_freq*self.t + self.phase_angle)
-    
-        
+            self._V = peak_voltage*np.sin(self._angular_freq*self._t + self._phase_angle)
+
 class Element():
     
-    class resistor():
+    class Resistor():
+        
         def __init__(self, resistance):
-            self.resistance = resistance
+            self._resistance = resistance
 
-    class capacitor():
+    class Capacitor():
         def __init__(self, capacitance):
             self.capacitance = capacitance
         
-    class inductor():
+    class Inductor():
         def __init__(self, inductance):
             self.inductance = inductance
+            
+class Circuit():
+    
+    '''
+    class Series():
+        def __init__(self, element1, element2):
+            self._element1 = element1
+            self._element2 = element2
+            
+    class Parallel():
+        def __init__(self, element1, element2):
+            self._element1 = element1
+            self._element2 = element2
+      '''
+
+    class Mesh():
+        
+        def __init__(self):
+            self._node_voltage = []
+            self._element_current = []
+            self._temp_dict = {}
+            
+        def add(self, element, node1, node2):
+            self._node1 = node1
+            self._node2 = node2
+            self._element = element
+            
+            # To add condition elem1 < > elem2
+            if len(self._node_voltage) == 0 and len(self._node_voltage) == 0:
+                
+                if type(self._element) == CurrentSource.DC or type(self._element) == CurrentSource.AC:
+                    self._temp_dict[str(self._node1) + str(self._node2)] = self._element._I
+                    temp_dict_copy = self._temp_dict.copy()
+                    self._element_current.append(temp_dict_copy)
+                    self._temp_dict.clear()
+                    
+                    self._temp_dict[str(self._node1) + str(self._node2)] = None
+                    temp_dict_copy = self._temp_dict.copy()
+                    self._node_voltage.append(temp_dict_copy)
+                    self._temp_dict.clear()
+                    
+                elif type(self._element) == VoltageSource.DC or type(self._element) == VoltageSource.AC:
+                    self._temp_dict[str(self._node1) + str(self._node2)] = self._element._V
+                    temp_dict_copy = self._temp_dict.copy()
+                    self._node_voltage.append(temp_dict_copy)
+                    self._temp_dict.clear()
+                    
+                    self._temp_dict[str(self._node1) + str(self._node2)] = None
+                    temp_dict_copy = self._temp_dict.copy()
+                    self._element_current.append(temp_dict_copy)
+                    self._temp_dict.clear()
+                
+            if self._node_voltage[0]{'01'} != None:
+                #vs
+            elif self._element_current[0]{'01'} != None:
+                #cs
+                
+                
+            if type(self._element) == Element.Resistor:
+                
